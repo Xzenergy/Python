cf60ace513812446cd8f7ffc44a74f115b5a53ba
Xzenergy <citadel.chase@gmail.com>
2024-09-01 14:07:43 -0700

    first commit

---
+++ CHOOSE_UR_OWN_ADV.py
@@ -0,0 +1,7 @@
+name = input("Type your name: ")
+print("Welcome", name, "to this adventure!")
+
+answer = input("""You are on a dirt road, it has come to an end and you can go left or right. 
+               Which way would you like to go? """).lower()
+
+if answer == "left"
